70d7a8dfe850a05a711b8e8aff9d6cf116e638ed
Tsubo Yuki <tytt3462@gmail.com>
2023-11-20 22:44:54 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -12,7 +12,22 @@ res=0
 
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+#out2="15\n -15\n 120\n"
+#if ["${out}" = "${out2}"]; then
+#	[ "$res" = 0 ] && echo OK
+ #       exit $res
+#else
+	#ng ${LINENO}
+#fi
+#[ "${out}" = 15 ] || ng ${LINENO}
+if [ "${out}" = 15 ] && [ "${out}" = -15 ] && [ "${out}" = 120 ]; then
+	echo ${LINENO}
+     #ng ${LINENO}
+fi
+
+	     
 
 [ "$res" = 0 ] && echo OK
 exit $res
+
+#-15 ||120 || 8.333333333333332  || ng ${LINENO}
