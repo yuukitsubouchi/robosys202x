cf259d69880ea7a0ce85f79efd0544c2e1655978
Tsubo Yuki <tytt3462@gmail.com>
2023-11-23 13:08:03 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -14,13 +14,13 @@ res=0
 out=$(seq 5 | ./plus)
 #[ "${out}" = 15 ] || ng ${LINENO}
 if [ "${out}" = 15 ] && [ "${out}" = -15 ] && [ "${out}" = 120 ] && [ "${out}" = 55 ]; then
-	echo ${LINENO}
-     #ng ${LINENO}
+	ng ${LINENO}
+     #echo ${LINENO}
 fi
 
 out2=$(seq 10 | ./plus)
 if [ "${out2}" = 55 ] && [ "${out2}" = -55 ] && [ "${out2}" = 3628800 ] && [ "${out}" = 385 ]; then
-        echo ${LINENO}
+        ng ${LINENO}
 fi
 	    
 
